eed834db50104d69048a0862fea190a8ed9929c7
Kark Leo <v.malykhin.ff@gmail.com>
2018-09-21 00:59:29 +0300

    Scritp don for testing

---
+++ js/actions.js
@@ -0,0 +1,41 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  mainMenuOpen();
+  productOpen();
+});
+
+function mainMenuOpen() {
+  var menu = document.querySelectorAll('.mainMenu');
+  var menuButton = document.querySelectorAll('.mainMenu__button');
+
+  if (menuButton.length > 0) {
+    menuButton.forEach(function (mainButton) {
+
+      mainButton.addEventListener("click", function (event) {
+
+        this.classList.toggle('mainMenu__button_open');
+        menu.forEach(function(menuItem) {          
+          menuItem.classList.toggle('mainMenu_open');
+        });
+      });
+    });
+  }
+};
+
+function productOpen() {
+  var products = document.querySelectorAll('.productCase');
+
+  if (products.length > 0) {
+    products.forEach(function (product) {
+      product.addEventListener("click", function (event) {        
+        this.parentElement.querySelectorAll('.productCase_open').forEach(function (elem) {
+          elem.classList.remove('productCase_open')
+        });
+
+        this.classList.toggle('productCase_open');
+      });
+    });
+  }
+};
+
