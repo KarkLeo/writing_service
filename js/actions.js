b834f56319b0c3ba089956ab901b30da98c905a1
Kark Leo <v.malykhin.ff@gmail.com>
2018-09-21 20:32:11 +0300

    Fixed menu arrow

---
+++ js/actions.js
@@ -0,0 +1,45 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  mainMenuOpen();
+  productOpen();
+});
+
+function mainMenuOpen() {
+  var menu = document.querySelectorAll('.mainMenu');
+  var menuButton = document.querySelectorAll('.mainMenu__button');
+
+  if (menuButton.length > 0) {
+    menuButton.forEach(function (mainButton) {
+
+      mainButton.addEventListener("click", function (event) {
+
+        this.classList.toggle('mainMenu__button_open');
+        menu.forEach(function(menuItem) {          
+          menuItem.classList.toggle('mainMenu_open');
+        });
+      });
+    });
+  }
+};
+
+function productOpen() {
+  var products = document.querySelectorAll('.productCase');
+  var productsName = document.querySelectorAll('.productCase__name');
+
+  if (products.length > 0) {
+    productsName.forEach(function (productName) {
+      productName.addEventListener("click", function (event) { 
+        var thisOpened = this.parentElement.classList.contains('productCase_open');       
+        this.parentElement.parentElement.querySelectorAll('.productCase_open').forEach(function (elem) {
+          elem.classList.remove('productCase_open')
+        });
+
+        if (!thisOpened) {
+          this.parentElement.classList.add("productCase_open")
+      }
+      });
+    });
+  }
+};
+
